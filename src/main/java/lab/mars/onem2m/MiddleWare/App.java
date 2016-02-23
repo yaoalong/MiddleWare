f24d6533376158e5c81b710d54753712ef37995a
yaoalong <sishimei@163.com>
2016-02-23 13:09:13 +0800

    first commit

---
+++ src/main/java/lab/mars/onem2m/MiddleWare/App.java
@@ -0,0 +1,13 @@
+package lab.mars.onem2m.MiddleWare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
